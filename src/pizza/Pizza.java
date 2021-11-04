89db25c45050765d2add2dd67c00ff80569e6538
Gilberto <18030244@itesa.edu.mx>
2021-11-03 19:16:09 -0600

    Base2

---
+++ src/pizza/Pizza.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizza;
+
+/**
+ *
+ * @author herfr
+ */
+public class Pizza {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        P_Inicio o = new P_Inicio();
+        o.setVisible(true);
+    }
+    
+}
